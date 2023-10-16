41d8da63abad26a0bf6fa8087422069748953a0e
marine-rle <120376794+marine-rle@users.noreply.github.com>
2023-10-16 19:45:28 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
                 <a href="{{ route('reseau.index') }}">{{ __('Listes des réseaux') }}</a>
             </div>
             <div>
-                {{ __('Vous naviguez en') }} [{{ session('locale') }}] [{{ App::getLocale() }}]
+
                 {{-- <a href="{{ route('language.change', ['code_iso' => 'fr']) }}">{{ __('French') }}</a> --}}
                 {{-- <a href="{{ route('language.change', ['code_iso' => 'en']) }}">{{ __('English') }}</a> --}}
             </div>
