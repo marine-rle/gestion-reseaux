f9f05531a93e14465c79be5e1380e32f033a7701
marine-rle <120376794+marine-rle@users.noreply.github.com>
2023-10-16 16:59:12 +0200

    Remise à la version d'avant

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,42 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <title>@yield('title', 'Formation Laravel 10')</title>
+</head>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+<body>
+  <div class="container">
+    <nav class="pb-5">
+      <div>
+        <a href="{{ route('ordinateur.index') }}">{{ __('Listes des ordinateurs') }}</a>
+        <a href="{{ route('serveur.index') }}">{{ __('Listes des serveurs') }}</a>
+        <a href="{{ route('reseau.index') }}">{{ __('Listes des réseaux') }}</a>
+      </div>
+      <div>
+        {{ __('Vous naviguez en') }} [{{ session('locale') }}] [{{ App::getLocale() }}]
+        <a href="{{ route('language.change', ['code_iso' => 'fr']) }}">{{ __('French') }}</a>
+        <a href="{{ route('language.change', ['code_iso' => 'en']) }}">{{ __('English') }}</a>
+      </div>
+      <form method="POST" action="{{ route('logout') }}">
+        @csrf
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+        <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
+                            this.closest('form').submit();">
+          {{ __('Log Out') }}
+        </x-dropdown-link>
+      </form>
+    </nav>
+    @yield('content')
+  </div>
+</body>
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
 </html>
